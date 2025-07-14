51396e50ebd05238ac41d404246c2b90f84175fa
jsstoni <jsstoniha@gmail.com>
2025-07-13 20:51:38 -0400

    Add outline variant and style button icons consistently

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,22 +3,27 @@ import type React from 'react';
 import { cloneElement, isValidElement, type ReactElement } from 'react';
 import { cn } from '@/lib/utils';
 
-const buttonVariants = cva('flex items-center gap-2 rounded-lg font-medium', {
-  variants: {
-    variant: {
-      default:
-        'border border-[#333333] bg-linear-to-tl from-[#262626] to-[#444444] text-shadow-xs text-white shadow-xs shadow-xs hover:bg-linear-to-bl',
-      ghost: 'hover:bg-accent',
+const buttonVariants = cva(
+  'inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg font-medium',
+  {
+    variants: {
+      variant: {
+        default:
+          'border border-[#333333] bg-linear-to-tl from-[#262626] to-[#444444] text-shadow-xs text-white shadow-xs shadow-xs hover:bg-linear-to-bl',
+        outline: 'border',
+        ghost: 'hover:bg-accent',
+      },
+      size: {
+        default: 'h-9 px-4 py-2 text-sm has-[>svg]:px-3 [&_svg]:size-5',
+        icon: 'size-12',
+      },
     },
-    size: {
-      default: 'h-9 px-4 py-2 text-sm has-[>svg]:px-3',
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-});
+);
 
 type ButtonProps = {
   asChild?: boolean;
